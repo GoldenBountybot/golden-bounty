2c2b1c49e79c4ad3b7bdd9b5f117e15e15b904f3
Alamin Molla <bekaryt7525@gmail.com>
2026-07-17 06:22:49 +0000

    File changes

--- src/components/wildbounty/useWildBounty.js
+++ src/components/wildbounty/useWildBounty.js
@@ -97,7 +97,7 @@ export function useWildBounty() {
     }
 
     if (stepWin > 0) {
-      const slow = cascadeCount >= 1 ? 1.4 : 1;
+      const slow = cascadeCount >= 1 ? 1.6 : 1.2;
       setCascadeSlow(slow);
       sfx.win(cascadeCount);
       const newTotal = totalWin + stepWin;
