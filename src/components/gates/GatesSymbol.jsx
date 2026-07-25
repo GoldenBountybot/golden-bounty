22758d40c74c914b8f448516253840e74bd77d9e
Alamin Molla <bekaryt7525@gmail.com>
2026-07-25 17:18:41 +0000

    File changes

--- src/components/gates/GatesSymbol.jsx
+++ src/components/gates/GatesSymbol.jsx
@@ -3,17 +3,17 @@ import { isMult, multValue } from '@/lib/gatesEngine';
 
 // AI-generated symbol images matching the real Gates of Olympus game screenshots
 const SYM_IMG = {
-  zeus:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/1e4f4872c_generated_image.png',
-  crown:     'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/732191668_generated_image.png',
-  hourglass: 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/18c64007b_generated_image.png',
-  ring:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/6f31611ef_generated_image.png',
-  goblet:    'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/37a8458f3_generated_image.png',
-  red:       'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/9b3c34b0e_generated_image.png',
-  blue:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/634ba6f60_generated_image.png',
-  green:     'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/bbd709972_generated_image.png',
-  yellow:    'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/ec54fd8b6_generated_image.png',
-  scatter:   'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/7e8af7ca0_generated_image.png',
-  mult:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/17772ef4b_generated_image.png',
+  zeus:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/11cb6ed14_generated_image.png',
+  crown:     'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/0c48e5eca_generated_image.png',
+  hourglass: 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/9da8d26cd_generated_image.png',
+  ring:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/14769a259_generated_image.png',
+  goblet:    'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/6bd320078_generated_image.png',
+  red:       'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/7ef12a1e3_generated_image.png',
+  blue:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/b1c522db7_generated_image.png',
+  green:     'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/1c36b0e6b_generated_image.png',
+  yellow:    'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/0f5404b04_generated_image.png',
+  scatter:   'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/f8921dee7_generated_image.png',
+  mult:      'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/0e0490cf3_generated_image.png',
 };
 
 export { SYM_IMG };
