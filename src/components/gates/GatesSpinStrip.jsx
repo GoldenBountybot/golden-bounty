1a0fa4ac12a17393993178cf6583c79dc205eae7
Alamin Molla <bekaryt7525@gmail.com>
2026-07-25 19:47:12 +0000

    File changes

--- src/components/gates/GatesSpinStrip.jsx
+++ src/components/gates/GatesSpinStrip.jsx
@@ -37,7 +37,7 @@ export default React.memo(function GatesSpinStrip({ turbo }) {
                 <img
                   src={img}
                   alt=""
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-contain"
                   draggable={false}
                   style={{ filter: 'blur(1.1px) brightness(0.8)', mixBlendMode: 'lighten' }}
                 />
