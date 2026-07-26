06b8ef41012b3224a5d2af7bab6d8ee0e965c847
Alamin Molla <bekaryt7525@gmail.com>
2026-07-26 18:08:22 +0000

    File changes

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -13,7 +13,7 @@ export default function BackButton({ href = '/', label = 'Back', className = '' 
         background: 'rgba(20,17,13,0.7)',
         color: '#e8c878',
         fontFamily: 'Georgia, serif',
-        marginTop: 30,
+        marginTop: 40,
       }}
     >
       <ArrowLeft className="w-4 h-4" />
