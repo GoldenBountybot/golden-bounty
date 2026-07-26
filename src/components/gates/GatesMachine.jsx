4ba5b05f4391f732e66b0dd11d7cff9b3f55775a
Alamin Molla <bekaryt7525@gmail.com>
2026-07-26 11:57:19 +0000

    File changes

--- src/components/gates/GatesMachine.jsx
+++ src/components/gates/GatesMachine.jsx
@@ -90,8 +90,9 @@ export default function GatesMachine() {
         <img src="https://media.base44.com/images/public/6a5698edffaa42a5b6637776/84fd16eb6_file_00000000e474820ba9fd196f5f5c9f06.png"
           alt="Zeus"
           className="pointer-events-none select-none"
-          style={{ position: 'absolute', right: '6%', bottom: 12, width: '46%', maxWidth: 180,
+          style={{ position: 'absolute', right: '6%', bottom: 8, width: '46%', maxWidth: 180,
             height: 'auto', objectFit: 'contain',
+            clipPath: 'inset(0 0 6px 0)',
             filter: 'drop-shadow(0 3px 6px rgba(0,0,0,0.9))' }} />
       </div>
 
