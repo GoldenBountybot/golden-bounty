9842b6d5f33765d37225a0c6a8b40d9ef1bff582
Alamin Molla <bekaryt7525@gmail.com>
2026-07-27 10:15:51 +0000

    ফুল স্ক্রিন ইমেজ কভার

--- src/pages/SlotGame.jsx
+++ src/pages/SlotGame.jsx
@@ -18,7 +18,7 @@ export default function SlotGame() {
       style={{
         height: "100dvh",
         backgroundImage: `url(${BG_URL})`,
-        backgroundSize: "112%",
+        backgroundSize: "cover",
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
         backgroundColor: "#1a120b",
