4603b3666550c393dcf8919564d297da1b415ead
Alamin Molla <bekaryt7525@gmail.com>
2026-07-27 13:40:25 +0000

    File changes

--- src/components/wildbounty/WildBountyMachine.jsx
+++ src/components/wildbounty/WildBountyMachine.jsx
@@ -40,8 +40,8 @@ export default function WildBountyMachine() {
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
           boxShadow: '0 18px 52px rgba(0,0,0,0.85)',
-          paddingTop: '8%',
-          paddingBottom: '16%',
+          paddingTop: '4%',
+          paddingBottom: '20%',
           paddingLeft: '0%',
           paddingRight: '0%',
         }}
