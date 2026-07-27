05e386e2c4523fc877448236cf023ac4b5af39fc
Alamin Molla <bekaryt7525@gmail.com>
2026-07-27 10:18:26 +0000

    File changes

--- src/pages/SlotGame.jsx
+++ src/pages/SlotGame.jsx
@@ -18,8 +18,8 @@ export default function SlotGame() {
       style={{
         height: "100dvh",
         backgroundImage: `url(${BG_URL})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
+        backgroundSize: "100% auto",
+        backgroundPosition: "center bottom",
         backgroundRepeat: "no-repeat",
         backgroundColor: "#1a120b",
       }}
