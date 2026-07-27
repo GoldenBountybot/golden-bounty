e489f4a3ba73c1178f87b7b850605ff2600c829e
Alamin Molla <bekaryt7525@gmail.com>
2026-07-27 14:36:11 +0000

    File changes

--- src/components/wildbounty/MultiplierBanner.jsx
+++ src/components/wildbounty/MultiplierBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Bull-skull + wooden banner image acts as the multiplier banner backdrop.
-const BANNER_IMG = 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/7be17c563_file_00000000b61481fabeec47bdbfaf5bfc.png';
+const BANNER_IMG = 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/4ac18429a_file_000000006e6481fa9ba283c788d4cc07.png';
 
 export default function MultiplierBanner() {
   return (
