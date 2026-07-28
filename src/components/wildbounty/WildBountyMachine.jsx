5a82e50df1e79fbfbfac1f735ccf3ae2b19359c5
Alamin Molla <bekaryt7525@gmail.com>
2026-07-28 04:34:23 +0000

    File changes

--- src/components/wildbounty/WildBountyMachine.jsx
+++ src/components/wildbounty/WildBountyMachine.jsx
@@ -116,7 +116,7 @@ export default function WildBountyMachine() {
       )}
 
       {/* Stats bar */}
-      <div className="-mt-12 pb-8">
+      <div className="-mt-20 pb-8">
         <InfoBar balance={g.balance} bet={g.bet} win={g.lastWin} />
 
         {/* Controls */}
