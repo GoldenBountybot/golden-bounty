176720ecdc2361f86af3f1cb311d1b4079ec70c7
Alamin Molla <bekaryt7525@gmail.com>
2026-07-29 18:50:10 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,7 @@ function App() {
       <QueryClientProvider client={queryClientInstance}>
         <Router>
           <ScrollToTop />
-          <TonConnectUIProvider manifestUrl="https://base44.app/api/apps/6a5698edffaa42a5b6637776/files/mp/public/6a5698edffaa42a5b6637776/a0c67c5f1_tonconnect-manifest.json">
+          <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
             <AuthenticatedApp />
           </TonConnectUIProvider>
         </Router>
