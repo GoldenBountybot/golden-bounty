b23a89949dfce62161d5ad60058c7a480aab0283
Alamin Molla <bekaryt7525@gmail.com>
2026-07-31 07:59:27 +0000

    File changes

--- src/pages/FortuneGems.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FortuneGemsMachine from '@/components/fortunegems/FortuneGemsMachine';
-
-export default function FortuneGems() {
-  return <FortuneGemsMachine />;
-}
