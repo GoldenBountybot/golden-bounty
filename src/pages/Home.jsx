29cf91786edfdbc04bde8317cf50f1544d1e878f
Alamin Molla <bekaryt7525@gmail.com>
2026-08-01 14:27:42 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -103,7 +103,7 @@ export default function Home() {
       </header>
 
       {/* Premium Western game banners */}
-      <div className="max-w-6xl mx-auto px-4 mt-0">
+      <div className="max-w-6xl mx-auto px-4 mt-2">
         <WesternGameBanners />
       </div>
 
