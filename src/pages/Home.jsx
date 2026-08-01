2e57baab7a117ae208a0dada116caf56f4eb453c
Alamin Molla <bekaryt7525@gmail.com>
2026-08-01 12:37:59 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex flex-col items-end gap-1.5 self-end mr-2 -translate-y-4">
+          <div className="flex flex-col items-end gap-1.5 self-end mr-2 -translate-y-2">
             <NotificationBell />
             <Link
               to="/dashboard"
