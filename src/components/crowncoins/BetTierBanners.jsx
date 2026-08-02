fdbe5d547e202e4f93c94be76891828fe19075cf
Alamin Molla <bekaryt7525@gmail.com>
2026-08-02 18:53:24 +0000

    File changes

--- src/components/crowncoins/BetTierBanners.jsx
+++ src/components/crowncoins/BetTierBanners.jsx
@@ -66,8 +66,8 @@ function Corner({ pos, children }) {
   const styles = {
     'top-left':    { top: '22px', left: '-2px' },
     'top-right':   { top: '22px', right: '-2px' },
-    'bottom-left': { bottom: '-34px', left: '-2px' },
-    'bottom-right':{ bottom: '-34px', right: '-2px' },
+    'bottom-left': { bottom: '-10px', left: '-2px' },
+    'bottom-right':{ bottom: '-10px', right: '-2px' },
   };
   return <div className={base} style={styles[pos]}>{children}</div>;
 }
