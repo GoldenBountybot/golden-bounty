b88a919ce5e1e7c92c3969af811ee8ddebb18f1b
Alamin Molla <bekaryt7525@gmail.com>
2026-08-02 02:21:30 +0000

    File changes

--- src/components/wildbounty/PlaqueBanner.jsx
+++ src/components/wildbounty/PlaqueBanner.jsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react';
 
 // Golden-frame wooden sign banner used as the win message backdrop.
 const BANNER_URL = 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/ded9f1015_file_00000000d1a0820eb551f775dc672260.png';
+const SKULL_URL = 'https://media.base44.com/images/public/6a5698edffaa42a5b6637776/fd2e464cf_file_000000002a3c820b808b197402106ca0.png';
 
 const PlaqueBanner = forwardRef(function PlaqueBanner({ children, className = '', style = {}, glow = false }, ref) {
   return (
@@ -23,6 +24,14 @@ const PlaqueBanner = forwardRef(function PlaqueBanner({ children, className = ''
       <div className="absolute inset-0 flex items-center justify-center px-[14%]">
         {children}
       </div>
+      {/* Decorative steer-skull topper sitting on the top border */}
+      <img
+        src={SKULL_URL}
+        alt=""
+        className="absolute left-1/2 -translate-x-1/2 -top-1 w-[42%] max-w-[180px] h-auto select-none pointer-events-none z-20"
+        draggable={false}
+        style={{ mixBlendMode: 'screen', filter: 'brightness(1.05) saturate(1.05)' }}
+      />
     </div>
   );
 });
