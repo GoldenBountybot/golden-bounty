860d53c2951fdb30b7f8de248a681164d19490fa
Alamin Molla <bekaryt7525@gmail.com>
2026-08-03 14:33:51 +0000

    File changes

--- src/pages/Airdrop.jsx
+++ src/pages/Airdrop.jsx
@@ -193,7 +193,7 @@ export default function Airdrop() {
        >
          <img src={BOUNTY_LOGO} alt="Bounty" className="w-9 h-9 shrink-0" style={{ mixBlendMode: 'screen' }} />
          <p className="text-[12px] font-semibold leading-snug" style={{ color: 'rgba(255,255,255,0.85)' }}>
-           {t("Token will be listed in Quarter 4 2026 on top-tier exchanges.")}
+           {t("Token Will Be Listed In Quarter 4 2026 On Top-Tier Exchanger")}
          </p>
        </div>
       </div>
