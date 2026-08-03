a12ad4e9848cda9618a2d0afc137be6e5a9869c6
Alamin Molla <bekaryt7525@gmail.com>
2026-08-03 12:44:08 +0000

    File changes

--- src/components/argonauts/argonautsEngine.js
+++ src/components/argonauts/argonautsEngine.js
@@ -98,7 +98,7 @@ export function reelWeights(reelIndex, freeSpins) {
     return w;
   }
   const w = { ...BASE_WEIGHTS };
-  if (reelIndex >= 1 && reelIndex <= 3) w.scatter = 5;
+  if (reelIndex >= 1 && reelIndex <= 3) w.scatter = 9;
   else delete w.scatter;
   return w;
 }
