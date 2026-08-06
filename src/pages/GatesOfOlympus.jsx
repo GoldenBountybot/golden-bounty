9cea0e47a7bf2dd31cbd525011b80b2685be6ad7
Alamin Molla <bekaryt7525@gmail.com>
2026-08-06 16:56:07 +0000

    File changes

--- src/pages/GatesOfOlympus.jsx
+++ src/pages/GatesOfOlympus.jsx
@@ -15,7 +15,7 @@ export default function GatesOfOlympus() {
         title="Gates of Olympus"
         assets={GATES_ASSETS}
         onDone={() => setReady(true)}
-        bgImage={GAME_BG.gates}
+        bgImage="https://media.base44.com/images/public/6a5698edffaa42a5b6637776/1f0dcd8e1_file_00000000534882308373132046ad84c6.png"
       />
     );
   }
