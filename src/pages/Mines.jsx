85b0e78a9773b68ba9cd910d05a0feadb8ef01d4
Alamin Molla <bekaryt7525@gmail.com>
2026-08-10 07:30:07 +0000

    File changes

--- src/pages/Mines.jsx
+++ src/pages/Mines.jsx
@@ -189,7 +189,7 @@ export default function Mines() {
     setRevealedOrder([]);
     setPot(1);
     setLastWin(0);
-    setForceFirstMine(serverWinRef.current === 0 || Math.random() > ((rtp ?? 50) / 100) * 0.6);
+    setForceFirstMine(serverWinRef.current === 0 || Math.random() > ((rtp ?? 50) / 100) * 0.7);
     setPhase('playing');
     setMessage(`Find ${safe} gold bars · dodge ${mines} TNT`);
   };
